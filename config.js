5d0ee430743125f42773ceeb5b385fae0642587c
WZ BOT OFFICIAL <gamingwan92@gmail.com>
2022-04-06 17:11:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 global.DeveloperMode = 'false' //true atau false, true berati iya false berati tidak Moga paham
 global.linkGC = [] // link gc lu
 global.channelYT = [] // link yt lu klo g ad g ush di isi
-global.owner = ['628818770766'] // masuk in no lu sebagai owner
-global.mods = ['6283897390164'] // masuk in no moderator ny
+global.owner = ['6287752430231'] // masuk in no lu sebagai owner
+global.mods = ['62882016646288'] // masuk in no moderator ny
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // ini buat premium
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -37,8 +37,8 @@ global.author = 'Zevano'
 
 global.multiplier = 36 // ini butuh berapa xp buat levelup 
 
-global.botwm = ['© nayla'] // watermark bot
-global.oname = ['Zevano'] // nama owner nya
+global.botwm = ['© RIZKY BOTZ'] // watermark bot
+global.oname = ['RIZKY STORE'] // nama owner nya
 
 let chalk = require('chalk')
 let file = require.resolve(__filename)
